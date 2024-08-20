1594f4f294aeaaa4d31342495e3f2abaf794712b
多bug的啸天犬 <morebugofdog@outlook.com>
2024-08-20 19:17:46 +0800

    fix bugs

--- src/components/body/home/ProjectCard/index.tsx
+++ src/components/body/home/ProjectCard/index.tsx
@@ -1,51 +1,61 @@
-import React from 'react';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Avatar, Card } from 'antd';
-import styles from './ProjectCard.module.scss';
+import React from "react";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card } from "antd";
+import styles from "./ProjectCard.module.scss";
 
-import Ttag from './Ttag'
+import Ttag from "./Ttag";
 
-import { Window } from '../../../../globals';
-
+import { Window } from "../../../../globals";
 
 const { Meta } = Card;
 
 interface ProjectCardProps {
-    coverURL: string;
-    title: string;
-    description: string;
-    projectID: string;
-    authorImg: string;
+  coverURL: string;
+  title: string;
+  description: string;
+  projectID: string;
+  authorImg: string;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ coverURL, title, description, projectID, authorImg }) => {
-    const openProject = () => {
-        Window.createWindow('player', `https://www.ccw.site/player/${projectID}`, "../src/null.js", title);
-    }
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  coverURL,
+  title,
+  description,
+  projectID,
+  authorImg,
+}) => {
+  const openProject = () => {
+    Window.createWindow(
+      "player",
+      `https://www.ccw.site/player/${projectID}`,
+      "../src/null.js",
+      title
+    );
+  };
 
-    return (
-        <Card className={styles.card}
-            cover={
-                <img
-                    alt="cover"
-                    src={coverURL}
-                />
-            }
-            actions={[
-                <SettingOutlined key="setting" />,
-                <EditOutlined key="edit" />,
-                <EllipsisOutlined key="ellipsis" />,
-            ]}
-            onClick={openProject}
-        >
-            <Meta
-                avatar={<Avatar src={authorImg} />}
-                title={title}
-                description={description}
-            />
+  return (
+    <Card
+      className={styles.card}
+      cover={<img alt="cover" src={coverURL} />}
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
+      onClick={openProject}
+    >
+      <Meta
+        avatar={<Avatar src={authorImg} />}
+        title={title}
+        description={description}
+      />
 
-        <Ttag tagname={["hello","word"]}/>
+      <Ttag tagname={["hello", "word"]} />
     </Card>
-);
-
+  );
+};
 export default ProjectCard;
